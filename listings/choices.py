97953d11804c5916fadf2a5b4bb406be0a7f3ee4
Ajeetgarg1998 <64128550+Ajeetgarg1998@users.noreply.github.com>
2020-06-01 22:30:16 +0530

    Add files via upload

---
+++ listings/choices.py
@@ -0,0 +1,53 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'Rs1,00,000',
+  '200000':'Rs2,00,000',
+  '300000':'Rs3,00,000',
+  '400000':'Rs4,00,000',
+  '500000':'Rs5,00,000',
+  '600000':'Rs6,00,000',
+  '700000':'Rs7,00,000',
+  '800000':'Rs8,00,000',
+  '900000':'Rs9,00,000',
+  '1000000':'Rs1M+',
+}
+
+state_choices = {
+        'AP':'Andhra Pradesh',
+        'AN':'Arunachal Pradesh',
+        'AS':'Assam',
+        'BR':'Bihar',
+        'CG':'Chattisgarh',
+        'CH':'Chandigarh',
+        'DL':'Delhi',
+        'DD':'Daman and Diu',
+        'GA':'Goa',
+        'GJ':'Gujrat',
+        'HP':'Himachal Pradesh',
+        'HR':'Haryana',
+        'JK':'Jammu and Kashmir',
+        'KA':'Karnataka',
+        'KL':'Kerala',
+        'LA':'Ladakh',
+        'MH':'Maharashtra',
+        'MP':'Madhya Pradesh',
+        'NL':'Nagaland',
+        'OD':'Odisha',
+        'PB':'Punjab',
+        'RJ':'Rajasthan',
+        'TN':'Tamil Nadu',
+        'UP':'Uttar Pradesh',
+        'WB':'West Bengal'
+}
